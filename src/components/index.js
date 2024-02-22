af255a24e9a2c6c09db37cf2dd6bd15446788e28
Rizil-Patel <128392275+Rizil-Patel@users.noreply.github.com>
2024-02-22 23:43:25 +0530

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Layout from '../Layout.jsx'
+import Home from './Home/Home.jsx'
+import About from './About/About.jsx'
+import Contact from './Contact/Contact.jsx'
+import User from './User/User.jsx'
+import Github from './Github/Github.jsx'
+
+
+export {Layout ,Home,About,Contact,User,Github}
